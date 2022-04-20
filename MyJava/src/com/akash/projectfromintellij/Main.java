267f884c6a2fdbb81f6e31fc26f22c6260799667
AkashPatil-1 <goodluckfriend009@gmail.com>
2022-04-20 19:01:11 +0530

    My First voter eligibility project

---
+++ MyJava/src/com/akash/projectfromintellij/Main.java
@@ -0,0 +1,19 @@
+package com.akash.projectfromintellij;
+
+import javax.swing.*;
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter Your Name: ");
+        String name = scanner.next();
+        System.out.println("Enter Your Age: ");
+        int age = scanner.nextInt();
+        if(age>=18)
+            System.out.println("Hi "+ name + " you are "+age+" years old and you are eligible for voting.");
+        else
+            System.out.println("Hi "+name+" you are "+age+" years old but not 18 so you are not eligible.");
+    }
+}
